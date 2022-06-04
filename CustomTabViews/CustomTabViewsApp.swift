725b16beecd20ab3e075d88c66362b17a1e512e7
agholson <87455899+agholson@users.noreply.github.com>
2022-06-04 12:11:16 -0400

    Initial Commit

---
+++ CustomTabViews/CustomTabViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewsApp.swift
+//  CustomTabViews
+//
+//  Created by Leone on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
